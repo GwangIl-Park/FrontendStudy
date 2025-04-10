45d2cb5934b936396a3cde9a20573f30faeba4c2
GwangIl-Park <rkasud0@gmail.com>
2025-04-10 17:24:16 +0900

    feat: react 강의 - css

---
+++ react-gymcoding/react-matirial-ui/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+
+  return (
+    <>
+        <Button variant='contained'>Button1</Button>
+        <Button variant='outline'>Button2</Button>
+        <Button variant='text'>Button3</Button>
+    </>
+  )
+}
+
+export default App
